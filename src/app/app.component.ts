c3e2717671f967187e4f7683f5d0e62eca3ccc1c
JP <jp.garcia.ortiz@gmail.com>
2021-03-30 19:57:34 +0200

    More

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'semaphore';
+  @ViewChild('tlights') tlights;
+  @ViewChild('code_input') code_input;
+
+  private timer: any;
+  public blocked: boolean = true;
+  public show_access: boolean = false;
+
+  onClickButton(): void {
+    this.blocked = !this.blocked;
+    this.tlights.setState(this.blocked ? 1 : 3);
+    clearInterval(this.timer);
+    if (!this.blocked) {
+       this.timer = setInterval(() => {
+          this.tlights.setRed();
+          clearInterval(this.timer);
+          this.blocked = true;
+       }, 4000)
+    }
+ }
+
+ onCodeCompleted(code: string): void {
+    //this.blocked = !this.blocked;
+    if(code == "1979")
+       this.show_access = false;
+    else
+       this.code_input.reset();
+ } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CodeInputModule } from 'angular-code-input';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TlightsComponent } from './tlights/tlights.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { TlightsComponent } from './tlights/tlights.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CodeInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
